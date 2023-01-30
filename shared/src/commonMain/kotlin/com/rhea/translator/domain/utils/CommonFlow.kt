531e9d2f523adcf8824633f5ade5f16e3d287628
Rhea <rheachhabra1011@gmail.com>
2023-01-30 22:59:17 +0530

    add comments

--- shared/src/commonMain/kotlin/com/rhea/translator/domain/utils/CommonFlow.kt
+++ shared/src/commonMain/kotlin/com/rhea/translator/domain/utils/CommonFlow.kt
@@ -2,6 +2,10 @@ package com.rhea.translator.domain.utils
 
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Flow<T> is the interface all asynchronous flows in Kotlin implement.
+ * While generic interfaces are lost in ios translation, generic classes are preserved.
+ */
 expect class CommonFlow<T>(source: Flow<T>): Flow<T>
 
 fun <T> Flow<T>.toCommonFlow() = CommonFlow(this)
--- shared/src/commonMain/kotlin/com/rhea/translator/domain/utils/CommonStateFlow.kt
+++ shared/src/commonMain/kotlin/com/rhea/translator/domain/utils/CommonStateFlow.kt
@@ -2,11 +2,6 @@ package com.rhea.translator.domain.utils
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
-
-/**
- * While generic type parameters of interfaces are lost in Ios translation,
- * generic type parameters of classes are preserved
- */
 expect class CommonStateFlow<T>(flow: StateFlow<T>): StateFlow<T>
 
 fun <T> StateFlow<T>.toCommonStateFlow() = CommonStateFlow(this)
