05eabec0406a441ebd409a4de91db0e12abd2d6f
Rhea <rheachhabra1011@gmail.com>
2023-02-24 22:58:16 +0530

    setup Translator Theme

--- androidApp/src/main/java/com/rhea/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/rhea/translator/android/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
