f5b1e880ae004422b1c3df718d4b326d7e2b0258
chhabrarhea <rheachhabra1011@gmail.com>
2023-03-29 16:04:52 +0530

    swap languages button ios

---
+++ iosApp/iosApp/translate/presentation/components/SwapLanguageButton.swift
@@ -0,0 +1,24 @@
+//
+//  SwapLanguageButton.swift
+//  iosApp
+//
+//  Created by Rhea Chhabra on 29/03/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwapLanguageButton: View {
+    var onClick: () -> Void
+    var body: some View {
+        Button(action: onClick){
+            Image(uiImage: UIImage(named: "swap_languages")!)
+        }
+    }
+}
+
+struct SwapLanguageButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SwapLanguageButton(onClick: {})
+    }
+}
